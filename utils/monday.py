986cc9626367afe614cb1cd40d948b5c6aea20a7
Gustavo Almeida de Jesus <66255218+SystemGuuh@users.noreply.github.com>
2024-04-03 19:19:36 +0000

    adding monday client filters

--- utils/monday.py
+++ utils/monday.py
@@ -5,23 +5,6 @@ import streamlit as st
 def load_api_key():
     return st.secrets['monday']['api_key']
 
-def getBoardResponse():
-    headers = {"Authorization": load_api_key()} 
-    apiUrl = "https://api.monday.com/v2"
-    query = '{boards {name id} }'
-    data = {'query': query}
-    response = requests.get(url=apiUrl, json=data, headers=headers)
-    return response.json()
-
-def getBoardData():
-    headers = {"Authorization": load_api_key()} 
-    apiUrl = "https://api.monday.com/v2"
-    query2 = '{boards(limit:1) { name id description items { name column_values{title id type text } } } }'
-    data = {'query' : query2}
-
-    r = requests.post(url=apiUrl, json=data, headers=headers)
-    return r.json()
-
 def makeRequestByQuery(query):
     headers = {"Authorization": load_api_key()} 
     query3 = query
